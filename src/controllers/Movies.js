7bba19e6226ec69357f2ea27385b50438da58956
Safna <safna.usa@gmail.com>
2021-01-26 10:12:23 -0600

    Added async add function

--- src/controllers/Movies.js
+++ src/controllers/Movies.js
@@ -1,15 +1,22 @@
 const Movie = require("../models/Movie");
 
-const list = async (req, res) => {
-  try {
-    const movies = await Movie.find({});
-    res.json(movies);
-  } catch (e) {
-    console.error("Error fetching movies from database: ", e);
-    res.status(500).send("Unable to fetch movies sorrys");
-  }
+const list = async(req, res) => {
+    try {
+        const movies = await Movie.find({});
+        res.json(movies);
+    } catch (e) {
+        console.error("Error fetching movies from database: ", e);
+        res.status(500).send("Unable to fetch movies sorrys");
+    }
+};
+
+const add = async(req, res) => {
+    const title = req.body.title;
+    const movie = new Movie({ title: title });
+    await movie.save();
+    res.json(movie);
 };
 
 module.exports = {
-  list,
+    list,
 };
